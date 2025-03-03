c84797d41d2fdc32126dd86ff02ce7f143a67437
hopeless <121128408+h0pe-less@users.noreply.github.com>
2025-03-03 23:58:36 +0200

    Updated asd.cpp
    Added a "number guess" minigame

--- asd.cpp
+++ asd.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
 int main()
 {
-  std::cout<<"ASMR";
+  std::cout << "ASMR" << std::endl;
+  
+  std::cout << "Let's play a quick guessing game!\n";
+    
+    std::srand(std::time(0));
+    int secretNumber = std::rand() % 10 + 1;
+    int guess;
+
+    std::cout << "Guess a number between 1 and 10: ";
+    std::cin >> guess;
+
+    if (guess == secretNumber) {
+        std::cout << "🎉 Congrats! You guessed it right!\n";
+    } else {
+        std::cout << "❌ Oops! The correct number was " << secretNumber << ". Try again!\n";
+    }
+    return 0;
 }
